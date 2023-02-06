f8f9fc08b4adbff758cc5663faa95b79515900f3
Sina Madadi <sinamadadi.work@gmail.com>
2023-02-07 00:26:12 +0330

    some editing on btns and cards.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -18,31 +18,33 @@ import { formDate, truncate } from '../utils/helpers'
 export const Posts = (res) => {
   const navigate = useNavigate()
   return (
-    <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 5 }}>
-      {res.posts.map((post, i) => (
-        <CCol key={i} xs>
-          <CCard onClick={() => navigate(`${res.adress}${post._id}`)}>
-            <CCardImage
-              orientation="top"
-              src={`http://localhost:3333/uploads/thumbnails/${post.thumbnail[0]}`}
-            />
-            <CCardBody>
-              <CCardTitle>{post.title}</CCardTitle>
-            </CCardBody>
-            <CListGroup flush>
-              <CListGroupItem>
-                <CBadge color={post.isAccept === 'accept' ? 'success' : 'danger'}>
-                  {post.isAccept}
-                </CBadge>
-              </CListGroupItem>
-              <CListGroupItem>{truncate(post.body, 15)}</CListGroupItem>
-            </CListGroup>
-            <CCardFooter>
-              <small className="text-medium-emphasis">{formDate(post.createdAt) }</small>
-            </CCardFooter>
-          </CCard>
-        </CCol>
-      ))}
-    </CRow>
+    <CRow xs={{ cols: 1, gutter: 4 }} md={{ cols: 3 }}>
+      {
+        res.posts.map((post, i) => (
+          <CCol key={i} xs>
+            <CCard onClick={() => navigate(`${res.adress}${post._id}`)} className="h-100">
+              <CCardImage
+                orientation="top"
+                src={`http://localhost:3333/uploads/thumbnails/${post.thumbnail[0]}`}
+              />
+              <CCardBody>
+                <CCardTitle>{post.title}</CCardTitle>
+              </CCardBody>
+              <CListGroup flush>
+                <CListGroupItem>
+                  <CBadge color={post.isAccept === 'accept' ? 'success' : 'danger'}>
+                    {post.isAccept}
+                  </CBadge>
+                </CListGroupItem>
+                <CListGroupItem>{truncate(post.body, 15)}</CListGroupItem>
+              </CListGroup>
+              <CCardFooter>
+                <small className="text-medium-emphasis">{formDate(post.createdAt)}</small>
+              </CCardFooter>
+            </CCard>
+          </CCol>
+        ))
+      }
+    </CRow >
   )
 }
