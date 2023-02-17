ad7c755328c2eb37c9593e18455fb297e9cfca50
Sina Madadi <sinamadadi.work@gmail.com>
2023-02-17 20:38:21 +0330

    some changes !

--- src/views/tourDashboard/withdrawList.js
+++ src/views/tourDashboard/withdrawList.js
@@ -2,43 +2,6 @@ import React from 'react'
 import { CCard, CCol, CRow, CTable, CTableBody, CTableDataCell, CTableHead, CTableHeaderCell, CTableRow } from '@coreui/react';
 
 const withdrawList = () => {
-    // return (
-    //     <CCard>
-    //         <CRow className="p-2">
-    //             <CCol xs={12} md={6} xl={6}>
-    //                 <CTable striped>
-    //                     <CTableHead>
-    //                         <CTableRow>
-    //                             <CTableHeaderCell scope="col">#</CTableHeaderCell>
-    //                             <CTableHeaderCell scope="col">Class</CTableHeaderCell>
-    //                             <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-    //                             <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
-    //                         </CTableRow>
-    //                     </CTableHead>
-    //                     <CTableBody>
-    //                         <CTableRow>
-    //                             <CTableHeaderCell scope="row">1</CTableHeaderCell>
-    //                             <CTableDataCell>Mark</CTableDataCell>
-    //                             <CTableDataCell>Otto</CTableDataCell>
-    //                             <CTableDataCell>@mdo</CTableDataCell>
-    //                         </CTableRow>
-    //                         <CTableRow>
-    //                             <CTableHeaderCell scope="row">2</CTableHeaderCell>
-    //                             <CTableDataCell>Jacob</CTableDataCell>
-    //                             <CTableDataCell>Thornton</CTableDataCell>
-    //                             <CTableDataCell>@fat</CTableDataCell>
-    //                         </CTableRow>
-    //                         <CTableRow>
-    //                             <CTableHeaderCell scope="row">3</CTableHeaderCell>
-    //                             <CTableDataCell colSpan={2}>Larry the Bird</CTableDataCell>
-    //                             <CTableDataCell>@twitter</CTableDataCell>
-    //                         </CTableRow>
-    //                     </CTableBody>
-    //                 </CTable>
-    //             </CCol>
-    //         </CRow>
-    //     </CCard>
-    // )
     const columns = [
         {
             key: 'trackingNum',
